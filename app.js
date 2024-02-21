c4603b7182d4135cd2db47379ae46c8409bac56b
abrormamurov <mabrorbk@gmail.com>
2024-02-22 00:06:36 +0500

    5kun

---
+++ app.js
@@ -0,0 +1,20 @@
+// tdz - o'lik zona
+// scope
+
+// function oddAndEvent(a) {
+//     if (a % 2 == 0);
+//         console.log(juft);
+// else console.log( toq son);
+
+// }
+
+function myFunction(a, b) {
+  return a * b;
+  // a va b ni kopaytmasi qaytariladi
+}
+
+let x = my(4, 3);
+
+function my(a, b) {
+  return a * b;
+}
